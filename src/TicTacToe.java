91f162f363bb6c07c8b493f0a0eb4f179d18cd81
lukestringer <luke.stringer@uqconnect.edu.au>
2021-01-09 16:55:19 +1000

    Refamiliarised myself with Stage, Scene and Node tree things with a simple rectangle. Next up is figuring out GridPane or Paint and Color

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,48 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
-public class TicTacToe {
+import java.util.*;
+
+public class TicTacToe extends Application {
 
     public static void main(String[] args) {
-        
+        //launch works because TicTacToe extends Application
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // set up primary stage
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(500);
+
+        Rectangle rect = new Rectangle(10, 10, 50, 50);
+        rect.setFill(Paint.valueOf("red"));
+
+        GridPane root = new GridPane();
+
+        root.getChildren().addAll(rect);
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+
+
+
+        primaryStage.show();
     }
 }
+
+
+/*
+Useful resources:
+https://docs.oracle.com/javafx/2/get_started/hello_world.htm
+ */
